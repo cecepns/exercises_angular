432c7c03dcdd179a7e90a5b728a404903ff4991d
DESKTOP-CJOERL0\LENOVO <cecepns0429@gmail.com>
2021-11-02 16:29:31 +0700

    setup angular

---
+++ src/app/mockdata/mock-heroes.ts
@@ -0,0 +1,44 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1,
+    name: 'Hyodo Issei',
+    image: 'heroes.png' 
+  },
+  { id: 2, 
+    name: 'Kazuma',
+    image: 'heroes2.png' 
+  },
+  { id: 3, 
+    name: 'Rias Gremory',
+    image: 'heroes3.png' 
+  },
+  { id: 4, 
+    name: 'Akeno',
+    image: 'heroes.png' 
+  },
+  { id: 5, 
+    name: 'Mumen Rider',
+    image: 'heroes2.png' 
+  },
+  { id: 6, 
+    name: 'Saimochi',
+    image: 'heroes3.png' 
+  },
+  { id: 7, 
+    name: 'Simon Gurren',
+    image: 'heroes.png' 
+  },
+  { id: 8, 
+    name: 'Sakata Gintoki',
+    image: 'heroes2.png' 
+  },
+  { id: 9, 
+    name: 'Abdul',
+    image: 'heroes3.png' 
+  },
+  { id: 10, 
+    name: 'Keyaru',
+    image: 'heroes.png' 
+  }
+];
